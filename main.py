d29d8df165df39085edc71a8439b5044f8aa46b4
akmko <111931776+akmko@users.noreply.github.com>
2023-01-28 21:45:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,6 @@ def decode_jwt():
     """
     Check user token and return non-secret data
     """
-    assert False
     if not 'Authorization' in request.headers:
         abort(401)
     data = request.headers['Authorization']
